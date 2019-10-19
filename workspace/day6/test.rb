87f584895ff7d3af247393a1410b97ffdc46e8e2
yuya tsuno <2ch.yuya@gmail.com>
2019-10-19 18:01:27 +0900

    Web技術入門シリーズ課題、提出

---
+++ workspace/day6/test.rb
@@ -0,0 +1,33 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+# WEBrick::HTTPServlet::FileHandlerをWEBrick::HTTPServlet::ERBHandlerに変更する
+# 'test.html'を'test.html.erb'に変更する
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+# 課題用に追加
+server.mount('/not_self_consumption.cgi', WEBrick::HTTPServlet::CGIHandler, 'not_self_consumption.rb')
+
+server.mount('/bad.cgi', WEBrick::HTTPServlet::CGIHandler, 'bad.rb')
+
+
+server.start
